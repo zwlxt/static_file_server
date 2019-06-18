bfa68058355e3cafde73fc72d09b22178b0b7632
ZENG Weilin <zengweilin@live.com>
2019-06-18 09:29:45 +0800

    Add upload feature

--- main.go
+++ main.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
+
+type R struct {
+	Result string   `json:"result,omitempty"`
+	Files  []string `json:"files,omitempty"`
+}
 
 type config struct {
 	Root    string
 	Address string
-}
-
-type fileList struct {
-	Files []string `json:"files"`
 }
 
 func main() {
@@ -25,7 +28,8 @@ func main() {
 
 	fs := http.FileServer(http.Dir(c.Root))
 	mux.Handle("/files/", http.StripPrefix("/files/", fs))
-	mux.HandleFunc("/files", h.handleFiles())
+	mux.HandleFunc("/files", h.HandleFileListJSON())
+	mux.HandleFunc("/upload", h.HandleUploadFiles())
 
 	err := http.ListenAndServe(c.Address, mux)
 	if err != nil {
@@ -49,15 +53,24 @@ func loadConfig() config {
 	return c
 }
 
+func jsonResponse(w http.ResponseWriter, j R) {
+	b, _ := json.Marshal(j)
+	w.Write(b)
+}
+
 type handlers struct {
 	dir string
 }
 
-func (h *handlers) handleFiles() http.HandlerFunc {
+func (h *handlers) HandleUploadFiles() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "GET":
-			h.fileListJSON(w, r)
+			b, err := Asset("templates/upload.html")
+			if err != nil {
+				panic(err)
+			}
+			w.Write(b)
 		case "POST":
 			h.fileUpload(w, r)
 		default:
@@ -66,22 +79,43 @@ func (h *handlers) handleFiles() http.HandlerFunc {
 	}
 }
 
-func (h *handlers) fileListJSON(w http.ResponseWriter, r *http.Request) {
-	files, err := ioutil.ReadDir(h.dir)
-	if err != nil {
-		panic(err)
+func (h *handlers) HandleFileListJSON() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		files, err := ioutil.ReadDir(h.dir)
+		if err != nil {
+			panic(err)
+		}
+
+		fileNames := make([]string, len(files))
+
+		for i, f := range files {
+			fileNames[i] = f.Name()
+		}
+
+		jsonResponse(w, R{Files: fileNames})
 	}
-
-	fileNames := fileList{make([]string, len(files))}
-
-	for i, f := range files {
-		fileNames.Files[i] = f.Name()
-	}
-
-	b, _ := json.Marshal(fileNames)
-	w.Write(b)
 }
 
 func (h *handlers) fileUpload(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(16 * 1024 * 1024)
+	uploadFile, handler, err := r.FormFile("upload_file")
+	if err != nil {
+		jsonResponse(w, R{Result: err.Error()})
+		return
+	}
+	defer uploadFile.Close()
 
+	outputFile, err := os.OpenFile(filepath.Join(h.dir, handler.Filename),
+		os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		jsonResponse(w, R{Result: err.Error()})
+		return
+	}
+	defer outputFile.Close()
+	written, err := io.Copy(outputFile, uploadFile)
+	if err != nil {
+		jsonResponse(w, R{Result: err.Error()})
+		return
+	}
+	jsonResponse(w, R{Result: strconv.FormatInt(written, 10)})
 }
